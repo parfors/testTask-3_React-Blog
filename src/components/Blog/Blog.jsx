33a8278ef41a9d5a15a317a13efdc29d33e03033
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-11-13 21:21:30 +0200

    add infinite scrol

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -11,9 +11,15 @@ import { useState } from "react";
 import { getBlogs } from "redux/selector";
 import Pagination from "components/Pagination/Pagination";
 import { pagesAmount } from "helpers/pagesAmount";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { PlaceholderTitleStyled } from "components/HomePlaceholder/HomePlaceholderStyled";
+
+const LIMIT = 10;
 
 export default function Blog() {
+  // -----------------------------Pagination------------------------------
   const blogs = useSelector(getBlogs);
+  const [isInfinite, setIsInfinite] = useState(false);
   const [blogsPerPage, setBlogsPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
 
@@ -28,10 +34,29 @@ export default function Blog() {
   const selectHandler = (e) => {
     const value = e.target.value;
     if (value === "-") {
-      setBlogsPerPage(Infinity);
+      setIsInfinite(true);
       return;
     }
+    setIsInfinite(false);
     setBlogsPerPage(value);
+  };
+  // ------------------------------Infinite scroll--------------------------
+  const [blogData, setBlogData] = useState(blogs.slice(0, LIMIT));
+  const [visible, setVisible] = useState(LIMIT);
+  const [hasMore, setHasMore] = useState(true);
+
+  const fetchData = () => {
+    const newLimit = visible + LIMIT;
+    const dataToAdd = blogs.slice(visible, newLimit);
+
+    if (blogs.length > blogData.length) {
+      setTimeout(() => {
+        setBlogData([...blogData].concat(dataToAdd));
+      }, 1000);
+      setVisible(newLimit);
+    } else {
+      setHasMore(false);
+    }
   };
 
   return (
@@ -41,35 +66,65 @@ export default function Blog() {
           Количество постов на странице
           <BlogSelectStyled defaultValue={10} onChange={selectHandler}>
             {pagesAmount.map((el, index) => (
-              <OptionStyled
-                // {...(el === 10 ? { selected: true } : {})}
-                key={index}
-                value={el}
-              >
+              <OptionStyled key={index} value={el}>
                 {el}
               </OptionStyled>
             ))}
           </BlogSelectStyled>
         </BlogLabelStyled>
-
         <BlogListStyled>
-          {currentBlog.map(({ id, title, author, text, category, img }) => (
-            <Post
-              key={id}
-              id={id}
-              title={title}
-              author={author}
-              text={text}
-              category={category}
-              img={img}
-            />
-          ))}
+          {isInfinite ? (
+            <InfiniteScroll
+              dataLength={blogData.length} //This is important field to render the next data
+              next={fetchData}
+              hasMore={hasMore}
+              loader={
+                <PlaceholderTitleStyled>Loading...</PlaceholderTitleStyled>
+              }
+              endMessage={
+                <PlaceholderTitleStyled>
+                  Все посты загружены
+                </PlaceholderTitleStyled>
+              }
+            >
+              {blogData.map(({ id, title, author, text, category, img }) => (
+                <Post
+                  key={id}
+                  id={id}
+                  title={title}
+                  author={author}
+                  text={text}
+                  category={category}
+                  img={img}
+                />
+              ))}
+            </InfiniteScroll>
+          ) : (
+            <>
+              {" "}
+              {currentBlog.map(({ id, title, author, text, category, img }) => (
+                <Post
+                  key={id}
+                  id={id}
+                  title={title}
+                  author={author}
+                  text={text}
+                  category={category}
+                  img={img}
+                />
+              ))}
+            </>
+          )}
         </BlogListStyled>
-        <Pagination
-          blogsPerPage={blogsPerPage}
-          paginate={paginate}
-          totalBlogs={blogs.length}
-        />
+        {isInfinite ? (
+          <></>
+        ) : (
+          <Pagination
+            blogsPerPage={blogsPerPage}
+            paginate={paginate}
+            totalBlogs={blogs.length}
+          />
+        )}
       </BlogContainerStyled>
     </>
   );
