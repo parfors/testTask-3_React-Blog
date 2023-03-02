0a9fafd58b08fd2ce730236f5898b6f3086afc97
parfors <suprun.atom@gmail.com>
2023-03-02 16:24:14 +0200

    deploying

--- src/components/HomePlaceholder/HomePlaceholder.jsx
+++ src/components/HomePlaceholder/HomePlaceholder.jsx
@@ -4,14 +4,45 @@ import {
   PlaceholderImgWrapper,
   PlaceholderTitleStyled,
   ContainerStyled,
+  PlaceholderTextStyled,
+  PlaceholderSpanStyled,
 } from "components";
+import catImg from "../../images/placeholder.jpg";
 
 export default function HomePlaceholder() {
   return (
     <ContainerStyled>
       <PlaceholderTitleStyled>Добро пожаловать</PlaceholderTitleStyled>
+      <PlaceholderTextStyled>
+        Вы на странице одного из моих тестовых заданий. Проект не стилизирован,
+        его задачай было продемонстрировать мои навыки владение следующими
+        технологиями:{" "}
+        <PlaceholderSpanStyled>
+          React, React Router, Redux, StyleComponents, Node.js, Express,
+          MongoDB.
+        </PlaceholderSpanStyled>
+      </PlaceholderTextStyled>
+      <PlaceholderTextStyled>
+        В проекте реализовано FrontEnd и BackEnd части. Выполнена маршруризация
+        (публичные и закрытые маршруты), используется Redux с настроиным
+        persistom. В задании содержится функционал запроса и отображения статтей
+        по категориям, выполнена динамическая пагинация (отображения по 5, 10,
+        15, 20 статтей на странице). Регистрация и логинизация выполнена с
+        использованием JWT. После логинизации появляется возможность добавления
+        и удаления своих постов. При добавлении поста реализована возможность
+        подгрузить файл обложки (бесплатный ресурс деплоя блокирует эту
+        функцию).
+      </PlaceholderTextStyled>
+      <PlaceholderTextStyled>Спасибо за внимание,</PlaceholderTextStyled>
+      <PlaceholderTextStyled>
+        С уважением Супрун Александр
+      </PlaceholderTextStyled>
+      <PlaceholderTextStyled>
+        P.S. Техзадание было написано на русском языке, поэтому проект также
+        выполнен на русском.
+      </PlaceholderTextStyled>
       <PlaceholderImgWrapper>
-        <PlaceholderImgStyled src="https://via.placeholder.com/600x800" />
+        <PlaceholderImgStyled src={catImg} />
       </PlaceholderImgWrapper>
     </ContainerStyled>
   );
--- src/components/HomePlaceholder/HomePlaceholderStyled.jsx
+++ src/components/HomePlaceholder/HomePlaceholderStyled.jsx
@@ -1,8 +1,8 @@
 import styled from "styled-components";
 
 export const PlaceholderTitleStyled = styled.h2`
-  padding-top: 20px;
-  padding-bottom: 30px;
+  padding-top: 15px;
+  padding-bottom: 15px;
   margin: 0;
   text-align: center;
 `;
@@ -13,3 +13,14 @@ export const PlaceholderImgWrapper = styled.div`
   justify-content: center;
 `;
 export const PlaceholderImgStyled = styled.img``;
+
+export const PlaceholderTextStyled = styled.p`
+  max-width: 800px;
+  margin: 0 auto;
+  text-align: justify;
+  padding-bottom: 10px;
+`;
+
+export const PlaceholderSpanStyled = styled.span`
+  font-weight: 900;
+`;
