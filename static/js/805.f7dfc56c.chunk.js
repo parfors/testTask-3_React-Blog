eff9fde8424c5566e34ac7ba62288098f2afa37b
parfors <suprun.atom@gmail.com>
2023-03-02 15:13:18 +0200

    Updates

--- static/js/805.f7dfc56c.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_tusk_3_react_blog=self.webpackChunktest_tusk_3_react_blog||[]).push([[805],{9805:function(e,s,t){t.r(s);var c=t(4811),n=t(6145),r=t(7689),l=t(5785),a=t(184);s.default=function(){var e=(0,r.s0)();return(0,a.jsx)(a.Fragment,{children:(0,a.jsxs)(l.N0,{children:[(0,a.jsx)(c.g9,{onClick:function(){e("/")},children:"Back to Home"}),(0,a.jsx)(c.b2,{children:"404 Page is not found"}),(0,a.jsx)(n.bI,{children:(0,a.jsx)(n.Cv,{src:"https://via.placeholder.com/600x800"})})]})})}}}]);
-//# sourceMappingURL=805.f7dfc56c.chunk.js.map
